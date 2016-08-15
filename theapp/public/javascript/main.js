8afd115c3fe741345c13e3609b150ec5cf299bf2
William Pruden <williamprudeniv@gmail.com>
2016-08-15 00:54:05 -0700

    home page styling changes

--- theapp/public/javascript/main.js
+++ theapp/public/javascript/main.js
@@ -1,1 +1,18 @@
 console.log("Main js loaded");
+
+$(function() {
+    var win;
+    var checkConnect;
+    var $connect = $("#login");
+    var oAuthURL = '/auth/google';
+
+    $connect.click(function() {
+        win = window.open(oAuthURL, 'SomeAuthentication', 'scrollbars=yes,resizable=yes,top=175,left=450,width=500,height=400');
+    });
+
+    checkConnect = setInterval(function() {
+        if (!win || !win.closed) return;
+        clearInterval(checkConnect);
+        window.location.reload();
+    }, 100);
+});
