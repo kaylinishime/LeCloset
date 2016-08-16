a50258170063641f2a406c25c5eb2a989d8781f4
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-08-16 09:29:26 -0700

    Added /:id in main js

--- theapp/public/javascript/main.js
+++ theapp/public/javascript/main.js
@@ -51,7 +51,7 @@ $('#lets_do_this1').on('click', function() {
   if ($gender.length >= 4) {
     //persist to database
     console.log('it works');
-    window.location.href="/products"
+    window.location.href="/products/:id"
   }
   else {
     // show error to the user
