8c00db474d43cb351cf5a30ebddd0960389ea641
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-08-16 09:27:00 -0700

    To start day 2

--- theapp/controllers/products_controller.js
+++ theapp/controllers/products_controller.js
@@ -9,14 +9,14 @@ res.render('products', { user: req.user });
 
 function show (req, res, next) {
   var req = rp.get({
-      uri: "http://api.shopstyle.com/api/v2/retailers?pid=" + process.env.API_KEY,
+      uri: "http://api.shopstyle.com/api/v2/products?pid=" + process.env.API_KEY,
       json: true
   })
 
   req.then(data => {
     console.log(data)
     res.render('products', {
-      retailers: data.retailers,
+      products: data.products,
       user: req.user
     })
   })
