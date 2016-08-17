94a847ea53bb7544856590433e674e3a81916d9f
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-08-17 11:35:15 -0700

    before pulling from team

--- theapp/public/javascript/usersClick.js
+++ theapp/public/javascript/usersClick.js
@@ -1,3 +1,30 @@
+
 $( ".jacketbutton" ).click(function() {
-  alert( "Handler for .click() called." );
+  $( ".catA" ).hide( "fast" );
 });
+
+$( ".shirtbutton" ).click(function() {
+  $( ".catB" ).hide( "fast" );
+});
+
+$( ".dressbutton" ).click(function() {
+  $( ".catC" ).hide( "fast" );
+});
+
+$( ".trouserbutton" ).click(function() {
+  $( ".catD" ).hide( "fast" );
+});
+
+$( ".shoebutton" ).click(function() {
+  $( ".catE" ).hide( "fast" );
+});
+
+// When I have this active I am not able to do the above
+// $( ".allbutton" ).click(function() {
+//   $( ".dog" ).show( "fast" );
+
+// $( ".allbutton" ).click(function() {
+//   $( "#dog" ).show( "fast", function showNext() {
+//     $( this ).next( "#dog" ).show( "fast", showNext );
+//   });
+// });
