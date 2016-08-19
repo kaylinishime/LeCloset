a113bee306d23a4c00abdd03d2a3f76b69ef63bb
Kayli Nishime <kmnishime@ucdavis.edu>
2016-08-18 17:39:27 -0700

    THE FILTERS WORKgit push

--- public/javascript/usersClick.js
+++ public/javascript/usersClick.js
@@ -22,7 +22,7 @@ $('.topbutton').click(function() {
   $(".misc").hide('fast')
 })
 
-$('.bottomsbutton').click(function() {
+$('.bottombutton').click(function() {
   // Double check that everything is visible
   $(".tops").show('fast'),
   $(".bottoms").show('fast'),
@@ -39,10 +39,11 @@ $('.bottomsbutton').click(function() {
 $('.shoesbutton').click(function() {
     console.log('clicked')
   // Double check that everything is visible
-  // $(".tops").show('fast'),
-  // $(".bottoms").show('fast'),
-  // $(".accessories").show('fast'),
+  $(".tops").show('fast'),
+  $(".bottoms").show('fast'),
+  $(".accessories").show('fast'),
   $(".shoes").show('fast'),
+  $(".misc").show('fast')
   // $(".misc").show('fast')
   // Hide everything exept for Jackets
   $(".tops").hide('fast'),
@@ -51,116 +52,31 @@ $('.shoesbutton').click(function() {
   $(".misc").hide('fast')
 })
 
-$('.trouserbutton').click(function() {
+$('.accessoriesbutton').click(function() {
   // Double check that everything is visible
-  $(".catA").show('fast'),
-  $(".catB").show('fast'),
-  $(".catC").show('fast'),
-  $(".catD").show('fast'),
-  $(".catE").show('fast')
+  $(".tops").show('fast'),
+  $(".bottoms").show('fast'),
+  $(".accessories").show('fast'),
+  $(".shoes").show('fast'),
+  $(".misc").show('fast')
   // Hide everything exept for Jackets
-  $(".catA").hide('fast'),
-  $(".catB").hide('fast'),
-  $(".catC").hide('fast'),
-  $(".catE").hide('fast')
+  $(".tops").hide('fast'),
+  $(".bottoms").hide('fast'),
+  $(".shoes").hide('fast'),
+  $(".misc").hide('fast')
 })
 
-$('.shoebutton').click(function() {
+$('.misc').click(function() {
 
   // Double check that everything is visible
-  $(".catA").show('fast'),
-  $(".catB").show('fast'),
-  $(".catC").show('fast'),
-  $(".catD").show('fast'),
-  $(".catE").show('fast')
+  $(".tops").show('fast'),
+  $(".bottoms").show('fast'),
+  $(".accessories").show('fast'),
+  $(".shoes").show('fast'),
+  $(".misc").show('fast')
   // Hide everything exept for Jackets
-  $(".catA").hide('fast'),
-  $(".catB").hide('fast'),
-  $(".catC").hide('fast'),
-  $(".catD").hide('fast')
+  $(".tops").hide('fast'),
+  $(".bottoms").hide('fast'),
+  $(".accessories").hide('fast'),
+  $(".shoes").hide('fast')
 })
-// $('#shoes').on('click', function() {
-//   $('#shoes').removeClass('hide')
-//   $('#shirts').removeClass('hide')
-//   $('#hats').removeClass('hide')
-//   $('#pants').removeClass('hide')
-//   $('#shorts').removeClass('hide')
-//   $('#jacket').removeClass('hide')
-
-//   $('#jackets').addClass('hide')
-//   $('#shirts').addClass('hide')
-//   $('#hats').addClass('hide')
-//   $('#pants').addClass('hide')
-//   $('#shorts').addClass('hide')
-// })
-
-// $( "button" ).click(function() {
-//   var clickedButtonId = $(this).attr(button);
-
-//   var divList = $("section").toArray();
-
-//   divList.sort(function(a, b) {
-//    if (clickedButtonId === '.allbutton') {
-//     return $(a).find('.allbutton').text() > $(b).find('.allbutton').text() ? 1 : -1; ;
-//    }
-
-//   else if (clickedButtonId === '.jacketbutton') {
-//     return $(b).find('.jacketbutton').data('.jacketbutton') - +$(a).find('.jacketbutton').data('.jacketbutton');
-//    }
-
-//   else if (clickedButtonId === '.shirtbutton'){
-//     return $(b).find('.shirtbutton').data('.shirtbutton') - +$(a).find('.shirtbutton').data('.shirtbutton');
-//   }
-
-//   else if (clickedButtonId === '.dressbutton'){
-//     return $(b).find('.dressbutton').data('.dressbutton') - +$(a).find('.dressbutton').data('.dressbutton');
-//   }
-
-//   else if (clickedButtonId === '.trouserbutton'){
-//     return $(b).find('.trouserbutton').data('.trouserbutton') - +$(a).find('.trouserbutton').data('.trouserbutton');
-//   }
-
-//   else if (clickedButtonId === '.shoebutton'){
-//     return $(b).find('.shoebutton').data('.shoebutton') - +$(a).find('.shoebutton').data('.shoebutton');
-//   }
-
-
-//     });
-
-//     $("section").html(divList);
-
-//     $('button').removeClass('active');
-//     $(this).addClass('active');
-//     });
-
-
-
-// $( ".jacketbutton" ).click(function() {
-//   $( ".catA" ).hide( "fast" );
-// });
-
-// $( ".shirtbutton" ).click(function() {
-//   $( ".catB" ).hide( "fast" );
-// });
-
-// $( ".dressbutton" ).click(function() {
-//   $( ".catC" ).hide( "fast" );
-// });
-
-// $( ".trouserbutton" ).click(function() {
-//   $( ".catD" ).hide( "fast" );
-// });
-
-// $( ".shoebutton" ).click(function() {
-//   $( ".catE" ).hide( "fast" );
-// });
-
-// When I have this active I am not able to do the above
-// $( ".allbutton" ).click(function() {
-//   $( ".dog" ).show( "fast" );
-
-// $( ".allbutton" ).click(function() {
-//   $( "#dog" ).show( "fast", function showNext() {
-//     $( this ).next( "#dog" ).show( "fast", showNext );
-//   });
-// });
