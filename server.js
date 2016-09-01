38e059a32815bed93ffe80c4140d4149ade66d40
Kayli Nishime <kmnishime@ucdavis.edu>
2016-08-31 18:46:58 -0700

    deleted new relic

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var session = require('express-session');
 var passport = require('passport');
 var debug = require('debug')('app:http');
 var _ = require('underscore');
-require('newrelic');
+
 
 // load the env vars
 require('dotenv').load();
